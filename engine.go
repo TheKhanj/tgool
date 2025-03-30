2ee4e8395b5290ab55639dbaf76d8d0beb6fed03
Pooyan Khanjankhani <pooyankhan@gmail.com>
2025-03-30 15:08:55 +0330

    make engine api more versatile

--- engine.go
+++ engine.go
@@ -1,14 +1,8 @@
 package tgool
 
 import (
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
-	"sync"
-
 	tg "github.com/go-telegram-bot-api/telegram-bot-api/v5"
-	"github.com/sirupsen/logrus"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
 type Engine struct {
@@ -18,52 +12,19 @@ type Engine struct {
 	bot    *tg.BotAPI
 }
 
-func New(
-	port int,
+func NewEngine(
 	router *Router,
 	bot *tg.BotAPI,
 ) *Engine {
 	return &Engine{
 		router: router,
-		port:   port,
 		bot:    bot,
 	}
 }
 
-func (e *Engine) Listen() {
-	var wg sync.WaitGroup
-	wg.Add(1)
+func (e *Engine) HandleUpdates(updates tgbotapi.UpdatesChannel) {
+	chatsState := &chatsState{}
 
-	addr := fmt.Sprintf("0.0.0.0:%d", e.port)
-
-	updates := make(chan tg.Update, e.bot.Buffer)
-
-	router := http.NewServeMux()
-	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		bytes, err := io.ReadAll(r.Body)
-		if err != nil {
-			return
-		}
-
-		var update tg.Update
-		json.Unmarshal(bytes, &update)
-
-		updates <- update
-	})
-
-	server := http.Server{
-		Addr:    addr,
-		Handler: router,
-	}
-
-	go func() {
-		defer wg.Done()
-		msg := fmt.Sprintf("admin telegram server is listening on port %d", e.port)
-		logrus.Info(msg)
-		server.ListenAndServe()
-	}()
-
-	chatsState := new(chatsState)
 	for update := range updates {
 		ctx := context{
 			bot:        e.bot,
@@ -72,6 +33,4 @@ func (e *Engine) Listen() {
 		}
 		e.router.Route(&ctx)
 	}
-
-	wg.Wait()
 }
